8944c844dd15f358b7229be64a10c516b9d2afd8
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2020-06-01 12:54:51 +0700

    Fix hypothesis not loaded on 'strict-dynamic' CSP

--- background.js
+++ background.js
@@ -1,13 +1,35 @@
-function listener(details) {
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message);                    
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string                  
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return hashHex;
+}
+
+async function listener(details) {
+    // Filter only response from document URL to patch
+    if (details.url.length <= 0 || typeof details.documentUrl !== 'undefined') {
+        return {};
+    }
+
     let cspIdx = details.responseHeaders.findIndex(function (headeritem) {
         return headeritem.name.toLowerCase() == "content-security-policy" && headeritem.value.indexOf('script-src ') !== -1;
     });
     if (cspIdx != -1) {
+        var results = await browser.storage.local.get('hypothesisHash');
         var csp = details.responseHeaders[cspIdx];
         var patchedCsp = csp.value
-        .replace(/frame-src ([^;]+);/, "frame-src $1 hypothes.is;")
-        .replace(/script-src ([^;]+);/, "script-src $1 cdn.hypothes.is;")
-        .replace(/style-src ([^;]+);/, "style-src $1 cdn.hypothes.is;");
+        .replace(/default-src ([^;]+);/, "default-src hypothes.is $1;")
+        .replace(/frame-src ([^;]+);/, "frame-src hypothes.is $1;")
+        .replace(/script-src ([^;]+);/, "script-src cdn.hypothes.is $1 'sha256-" + results['hypothesisHash'] + "';")
+        .replace(/style-src ([^;]+);/, "style-src cdn.hypothes.is 'unsafe-inline' $1;");
 
         details.responseHeaders[cspIdx].value = patchedCsp;
     }
@@ -17,8 +39,19 @@ function listener(details) {
     };
 }
 
-browser.webRequest.onHeadersReceived.addListener(
-    listener,
-    { urls: [ "<all_urls>" ] },
-    [ "blocking", "responseHeaders" ]
-);
+/* ----- *
+*  Main
+* ----- */
+
+fetch('https://hypothes.is/embed.js')
+.then(response => response.text())
+.then(async function (script) {
+    var hypothesisHash = await sha256(script);
+    browser.storage.local.set({ hypothesisHash: hypothesisHash });
+
+    browser.webRequest.onHeadersReceived.addListener(
+        listener,
+        { urls: [ "<all_urls>" ] },
+        [ "blocking", "responseHeaders" ]
+    );
+});
--- hypothesis.js
+++ hypothesis.js
@@ -1,12 +1,15 @@
-(function() {
+(async function() {
     window.hypothesisConfig = function() {
         return {
             showHighlights: true,
             appType: 'bookmarklet'
         };
     };
+
+    var results = await browser.storage.local.get('hypothesisHash');
     var d = document,
         s = d.createElement('script');
     s.setAttribute('src', 'https://hypothes.is/embed.js');
+    s.setAttribute('hash', results['hypothesisHash']);
     d.body.appendChild(s)
 })();

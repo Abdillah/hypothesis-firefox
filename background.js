8b04b4ead6bb8cdf4424e896f73fd4d39251dd4d
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-07-16 07:14:12 +0700

    Remove already isolated patching CSP code

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-import { CspPatcher } from './src/csp/patcher.js';
+import { patchCspForHypothesis } from './src/functions.js';
 
 async function sha256(message) {
     // encode as UTF-8
@@ -30,32 +30,7 @@ async function onHeaderPassed(details) {
     if (cspIdx != -1) {
         var results = await browser.storage.local.get('hypothesisHash');
         var csp = details.responseHeaders[cspIdx];
-
-        var patcher = CspPatcher.create(csp.value);
-        patcher = patcher
-            .addHost('default-src', "https://hypothes.is")
-            .addHost('frame-src', "https://hypothes.is")
-            .addHost('script-src', "https://cdn.hypothes.is")
-            .addHost('style-src', "https://cdn.hypothes.is")
-            .addHost('style-src', "'unsafe-inline'")
-        ;
-
-        // When only default-src available, we must add CDN URL to it
-        if (!patcher.hasRule('script-src') || !patcher.hasRule('style-src')) {
-            patcher = patcher
-            .addHost('default-src', "https://cdn.hypothes.is")
-        }
-
-        if (patcher.hasHashRule('script-src') || patcher.hasNonceRule('script-src')) {
-            patcher = patcher
-            // Hash of inline hypothesisConfig textContent
-            .addHost('script-src', "'nonce-w9s09t'")
-            .addHost('script-src', `'sha256-${results['hypothesisHash']}'`)
-        }
-
-        var patchedCsp = patcher.toString();
-
-        details.responseHeaders[cspIdx].value = patchedCsp;
+        details.responseHeaders[cspIdx].value = patchCspForHypothesis(csp.value);
     }
 
     return {
